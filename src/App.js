e17f98e906733d92cb39e6c0f359cb1ca0088b91
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2019-09-28 11:56:48 -0500

    fix api url

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './screens/users/users';
+import { API_URL } from './conf';
+import { authProvider } from './services/auth/auth';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const dataProvider = jsonServerProvider(API_URL);
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} authprovider={authProvider}>
     <Resource name="users" list={UserList} />
   </Admin>
 );
-export default App;   
+
+export default App;
---
+++ src/conf.js
@@ -0,0 +1,3 @@
+
+
+export const API_URL = 'http://jsonplaceholder.typicode.com'
