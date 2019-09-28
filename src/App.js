e6460bcb6d0d963834b34e9064b6900b563083cc
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2019-09-28 11:10:32 -0500

    user screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
+// in src/App.js
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './screens/users/users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={UserList} />
+  </Admin>
+);
+export default App;   
