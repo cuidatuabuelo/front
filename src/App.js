dcd6914c59411b3118faac4c1e2f1c4d9aaf58c1
Jefferson Roa <jeffer.roabeltran@gmail.com>
2019-09-28 13:20:24 -0500

    add homePage

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './screens/users/users';
-
-import {landing } from './screens/landing/landing';
+import { landing } from './screens/landing/landing';
+import { homePage } from './screens/home/home';
 import { API_URL } from './conf';
 import { authProvider } from './services/auth/auth';
 
 
 const dataProvider = jsonServerProvider(API_URL);
 const App = () => (
-  <Admin dashboard={landing} dataProvider={dataProvider} authprovider={authProvider}>
+  <Admin dashboard={homePage} dataProvider={dataProvider} authprovider={authProvider}>
     <Resource name="users" list={UserList} />
   </Admin>
 );
